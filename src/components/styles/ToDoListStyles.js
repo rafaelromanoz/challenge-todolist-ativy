1dc7d178294c46a16b1ff7db0c4a60719c0a2813
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2022-03-06 16:40:05 -0300

    :lipstick: working in styles

--- src/components/styles/ToDoListStyles.js
+++ src/components/styles/ToDoListStyles.js
@@ -1,1 +1,18 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
+
+const TodoContainerCard = styled.section`
+  display: flex;
+  flex-flow: column wrap;
+  align-items: center;
+  margin: 15px;
+`;
+
+const TodoCard = styled.div`
+  border-radius: 5px;
+  border: solid 2px black;
+  &:hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export { TodoContainerCard, TodoCard };
